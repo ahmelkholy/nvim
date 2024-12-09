14edea86a47bcae06dac707c623a82cfb0798601
Ahmed Elkholy <ahmed_elkholy@f-eng.tanta.edu.eg>
2024-12-09 19:36:50 +0300

    "regular automatic update"

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,10 +10,6 @@ local map = vim.keymap.set
 -- Map ";" to ":" for easier command entry
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
--- Disable default Ctrl+D mapping
--- (Assuming you want to disable it, you can unmap it)
-map("n", "<C-d>", "<Nop>", { desc = "Disable default Ctrl+D mapping" })
-
 -- Map j to gj and k to gk for soft wrapping
 map('n', 'j', 'gj', { desc = 'Move down by display line' })
 map('n', 'k', 'gk', { desc = 'Move up by display line' })
@@ -29,3 +25,17 @@ map('n', '<C-q>', ':bd<CR>', { desc = 'Close current buffer' })
 
 -- Toggle file explorer
 map('n', '<C-e>', ':NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
+
+local cursors = require('vscode-multi-cursor')
+
+local k = vim.keymap.set
+k({ 'n', 'x' }, 'mc', cursors.create_cursor, { expr = true, desc = 'Create cursor' })
+k({ 'n' }, 'mcc', cursors.cancel, { desc = 'Cancel/Clear all cursors' })
+k({ 'n', 'x' }, 'mi', cursors.start_left, { desc = 'Start cursors on the left' })
+k({ 'n', 'x' }, 'mI', cursors.start_left_edge, { desc = 'Start cursors on the left edge' })
+k({ 'n', 'x' }, 'ma', cursors.start_right, { desc = 'Start cursors on the right' })
+k({ 'n', 'x' }, 'mA', cursors.start_right, { desc = 'Start cursors on the right' })
+k({ 'n' }, '[mc', cursors.prev_cursor, { desc = 'Goto prev cursor' })
+k({ 'n' }, ']mc', cursors.next_cursor, { desc = 'Goto next cursor' })
+k({ 'n' }, 'mcs', cursors.flash_char, { desc = 'Create cursor using flash' })
+k({ 'n' }, 'mcw', cursors.flash_word, { desc = 'Create selection using flash' })
