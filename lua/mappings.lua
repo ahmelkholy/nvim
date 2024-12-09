4d5fb5a79dc0617f229090d9f2a1e5e7c960fdbf
Ahmed Elkholy <ahmed_elkholy@f-eng.tanta.edu.eg>
2024-12-09 15:20:52 +0300

    "regular automatic update"

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,10 +11,21 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
 -- Disable default Ctrl+D mapping
+-- (Assuming you want to disable it, you can unmap it)
+map("n", "<C-d>", "<Nop>", { desc = "Disable default Ctrl+D mapping" })
 
 -- Map j to gj and k to gk for soft wrapping
 map('n', 'j', 'gj', { desc = 'Move down by display line' })
 map('n', 'k', 'gk', { desc = 'Move up by display line' })
 
 -- Custom mapping with remap option
-map('n', 'mcw', '<C-d>', { remap = true, desc = 'Custom mapping for Ctrl+D' })
+map('n', '<C-d>', 'mciw*<Cmd>nohl<CR>', { remap = true, desc = 'Custom delete word and highlight' })
+
+-- Save file quickly
+map('n', '<C-s>', ':w<CR>', { desc = 'Save file' })
+
+-- Close current buffer
+map('n', '<C-q>', ':bd<CR>', { desc = 'Close current buffer' })
+
+-- Toggle file explorer
+map('n', '<C-e>', ':NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
